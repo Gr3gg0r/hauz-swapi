6e0eb9e07d1b53c018baf139fafc8b013b93310c
Shahfiq Shah <48479663+Gr3gg0r@users.noreply.github.com>
2021-08-26 00:20:23 +0800

    Update planet.js
    Complete Planets.

--- src/controllers/planet.js
+++ src/controllers/planet.js
@@ -1,7 +1,9 @@
 const { getAllStarWarPlanet } = require("../services/planet-swapi");
+//Thankfully nodejs run the whole file first , so no double request .
+const { getAllStarwarsPeople } = require("../services/people-swapi");
 
 let planets = [];
-
+let people = [];
 //Non Blocking
 (async () => {
   const data = await getAllStarWarPlanet();
@@ -9,8 +11,28 @@ let planets = [];
   console.log("done");
 })();
 
+//Non blocking code
+(async () => {
+  const data = await getAllStarwarsPeople(); 
+  people.push(...data);
+})();
+
 const getPlanet = (req, res) => {
-  res.json({ count:planets.length ,data: planets });
+
+  let newPlanets = [...planets].map(({ residents, ...rest }) => {
+    let newRes = residents.map((f) => {
+      let id = f.split("/").slice(-2, -1);
+      try {
+        return people[id - 1].name;
+      } catch (e) {
+        console.log(id - 1);
+      }
+    });
+
+    return { ...rest, residents: newRes };
+  });
+
+  res.json({ count: planets.length, data: newPlanets });
 };
 
 module.exports = {
